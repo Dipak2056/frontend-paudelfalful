7e5cfec16c1e6ee0393f69bd02ec1a99c2d1a6e3
Dipak 2056 <paudelsantosh508@gmail.com>
2022-06-15 20:15:48 +1000

    capturing uncontrolled form data usin useRef

--- src/components/login-form/Loginform.jsx
+++ src/components/login-form/Loginform.jsx
@@ -1,18 +1,38 @@
 import React from "react";
+import { useRef } from "react";
 import { Container, Form, Button } from "react-bootstrap";
 import "./login.css";
-
+const initialState = {
+  email: "paudelsantosh508@gmail.com",
+  password: "Dipak1234",
+};
 export const LoginForm = () => {
+  const emailRef = useRef();
+  const passRef = useRef();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const email = emailRef.current.value;
+    const password = passRef.current.value;
+    if (!email || !password) {
+      return alert("Email and password must be filled before submitting");
+    }
+    console.log(email, password);
+  };
   return (
     <div>
       <Container>
         <div className="form-content mt-5">
           <h1>Welcome Back !</h1>
           <hr />
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email</Form.Label>
-              <Form.Control name="email" placeholder="Enter email" required />
+              <Form.Control
+                name="email"
+                ref={emailRef}
+                placeholder="Enter email"
+                required
+              />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -20,6 +40,7 @@ export const LoginForm = () => {
               <Form.Control
                 type="password"
                 name="password"
+                ref={passRef}
                 placeholder="Password"
                 required
               />
